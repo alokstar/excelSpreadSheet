cd558e13ab7d072c0ae686cf22a8f87e35b0fed3
alokstar <alokpandhare16@gmail.com>
2019-01-24 23:56:17 +0530

    setting up the basic project

---
+++ spreadSheet.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Alok on 2019-01-24.
+ */
+function createExcel() {
+    let rows = document.getElementById('row').value;
+    let columns = document.getElementById('column').value;
+    console.log(rows, columns);
+
+}
