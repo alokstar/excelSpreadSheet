dc6a90a93447e7064a81c68ab0de105d8dbc1cfe
alokstar <alokpandhare16@gmail.com>
2019-01-25 13:44:36 +0530

    adds the basic table with provided dimensions

--- spreadSheet.js
+++ spreadSheet.js
@@ -5,5 +5,22 @@ function createExcel() {
     let rows = document.getElementById('row').value;
     let columns = document.getElementById('column').value;
     console.log(rows, columns);
+    // let myArray = new Array(rows);
+    //
+    // for(let i = 0; i < 2; i++) {
+    //     myArray[i] = new Array(columns);
+    // }
+    //
+    // console.log(myArray);
 
+    for (let i = 0; i < rows; i++) {
+        let table = document.getElementById("myTable");
+        let row = table.insertRow(i);
+
+        for (let j = 0; j < columns; j++) {
+            let cell1 = row.insertCell(j);
+            (cell1).innerHTML = " ";
+            cell1.contentEditable = true;
+        }
+    }
 }
